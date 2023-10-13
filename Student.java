676f9f7513dd694a5d55da0afc7784ef0ca6724d
ChitranshGuha <143699694+ChitranshGuha@users.noreply.github.com>
2023-10-13 12:45:51 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+class dob {
+    int date, month, year;
+}
+public class Student {
+    int sem;
+    String enrollmentNo, name, emailid, password;
+    dob DOB = new dob();
+
+    void getdata() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("* Enter Information *");
+        System.out.print("Name:");
+        name = sc.next();
+        System.out.print("Enrollment number:");
+        enrollmentNo = sc.next();
+        System.out.print("Password:");
+        password = sc.next();
+        System.out.print("Email id:");
+        emailid = sc.next();
+        System.out.println("Date of Birth");
+        System.out.print("Date:");
+        DOB.date = sc.nextInt();
+        System.out.print("Month:");
+        DOB.month = sc.nextInt();
+        System.out.print("Year:");
+        DOB.year = sc.nextInt();
+        System.out.println("Current Semester:");
+        sem = sc.nextInt();
+    }
+
+    void showdata() {
+        System.out.println("Name: " + name);
+        System.out.println("Enrollment number: " + enrollmentNo);
+        System.out.println("Email id: " + emailid);
+        System.out.println("Date of Birth (dd mm yy): " + DOB.date + " " + DOB.month + " " + DOB.year);
+        System.out.println("Semester: " + sem);
+    }
+
+    int StuLogin(String enn, String pwd) {
+        if (enrollmentNo.equals(enn) && password.equals(pwd)) {
+            System.out.println("Login Complete");
+            return 1;
+        }
+        else {
+            System.out.println("Incorrect Credentials");
+            return 0;
+        }
+    }
+}
